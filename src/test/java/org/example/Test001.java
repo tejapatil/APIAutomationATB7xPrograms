c6ac570747cf74ae7acc266fb66571a6429c2df9
Admin <tejapatil53@gmail.com>
2024-11-10 00:30:26 +0530

    2

---
+++ src/test/java/org/example/Test001.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+}
