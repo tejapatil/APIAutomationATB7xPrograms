e2ba3ea6a03c06ec757aaa5604f530bf2aff1f6e
Admin <tejapatil53@gmail.com>
2024-11-10 01:21:18 +0530

    2

--- src/test/java/org/example/Test002.java
+++ src/test/java/org/example/Test002.java
@@ -5,7 +5,24 @@ import io.restassured.RestAssured;
 public class Test002 {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Rest assured Test case");
+        System.out.println("Get Request Demo");
+
+        // Gherkins Syntax
+//        given() - url, headers, body or paylaod
+//        when() - http methods - get, post, patch, put, delete
+//        then() - verify the response - er == ar
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                        .statusCode(200); //<201> but was <200>
+
+
 
     }
 }
